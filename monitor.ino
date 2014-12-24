bb1388f33bca868f2f9577445cbb66ba216dd774
Ed Cardoz <edgar.cardoz@gmail.com>
2014-12-24 13:22:47 -0600

    Update monitor.ino
    Se corrigieron errores en el buffer del puerto serial. 
    Se añadieron funciones para impresion de barras, porcentajes de los cores, y datos de memoria

--- monitor.ino
+++ monitor.ino
@@ -13,7 +13,8 @@ products from Adafruit!
 
 Written by Limor Fried/Ladyada  for Adafruit Industries.
 BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
+All text above, and the splash screen must be included in any 
+redistribution
 *********************************************************************/
 
 /********************************************************************
@@ -30,28 +31,14 @@ Image2GLCD.exe by Jirawat Kongkaen http://avrportal.com
 /********************************************************************
 Adapted by Edgar Casanova Cardoz - Diciembre 2014
 http://idun.com.mx
-ARDUTOP System monitor for PC Gamers
+Display bitmap images on PCD8544 GLCD
 
 ********************************************************************/
 
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
-#include <MemoryFree.h>  //not necesary
-
-// Please use a level shifter (ex. 74HC4050) if you're using a 5V Arduino
-// pin 7 - Serial clock out (SCLK)
-// pin 6 - Serial data out (DIN)
-// pin 5 - Data/Command select (D/C)
-// pin 4 - LCD chip select (CS)
-// pin 3 - LCD reset (RST)
-
-Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 3, 4);
-
-int potPin = 5;    // select the input pin for the potentiometer
-int ledPin = 13;   // select the pin for the LED
-int val = 0;       // variable to store the value coming from the sensor
-
+#include <MemoryFree.h>
 
 const unsigned char PROGMEM idun [] = {
 0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
@@ -104,7 +91,21 @@ const unsigned char PROGMEM idun [] = {
 0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0  
 };
 
-int led = 9;
+// Please use a level shifter (ex. 74HC4050) 
+//  if you're using a 5V Arduino
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+
+Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 3, 4);
+
+int potPin = 5;    // select the input pin for the potentiometer
+int ledPin = 13;   // select the pin for the LED
+int val = 0;       // variable to store the value coming from the sensor
+int led = 9;       // led azul del LCD
+
 void setup()   {
   pinMode(ledPin, OUTPUT); 
   Serial.begin(9600);
@@ -124,21 +125,37 @@ void setup()   {
 
 
 void loop() {
-  int t = 69;
-  char recurso[t];
+  int sizeStream = 20;
+  char stream[sizeStream];
   display.clearDisplay();
   display.setTextSize(1);
   display.setCursor(0,0);
   if(Serial.available()){
-    Serial.readBytes(recurso, 69);
-    imprimirTexto(recurso);
+    delay(10);
+    Serial.readBytes(stream, sizeStream);
+    //Serial.println(stream);
+    Serial.flush();
+    monitorBase();
     
-    display.fillRect(72, 40, 22, 7, WHITE);
+    printCpuPercent(35, 0, stream[0],  stream[1],  stream[2]);
+    //display.fillRect(60, 0,  20, 7, WHITE);    
+    drawBar(stream[0],  stream[1],  stream[2],  0);
     
-    drawBar(recurso[6], recurso[7], recurso[8], 0);
-    drawBar(recurso[17], recurso[18], recurso[19], 8);
-    drawBar(recurso[28], recurso[29], recurso[30], 16);
-    drawBar(recurso[39], recurso[40], recurso[41], 24);    
+    printCpuPercent(35, 8, stream[3],  stream[4],  stream[5]);
+    //display.fillRect(60, 8,  20, 7, WHITE);
+    drawBar(stream[3],  stream[4],  stream[5],  8);
+    
+    printCpuPercent(35, 16, stream[6],  stream[7],  stream[8]);
+    //display.fillRect(60, 16,  20, 7, WHITE);
+    drawBar(stream[6],  stream[7],  stream[8],  16);
+    
+    printCpuPercent(35, 24, stream[9],  stream[10], stream[11]);
+    //display.fillRect(60, 24,  20, 7, WHITE);
+    drawBar(stream[9],  stream[10], stream[11], 24);    
+    
+    printMem(35, 32, stream[12], stream[13], stream[14], stream[15]);
+    printMem(35, 40, stream[16], stream[17], stream[18], stream[19]);
+    
     display.display();
     Serial.end();    
     Serial.begin(9600);
@@ -149,8 +166,8 @@ void loop() {
 }
 
 void drawBar(char b1, char b2, char b3, int inicio_barra){
-  char buffer[4];  // Las siguientes 4 lineas fueron un autentico problema
-  buffer[0] = b1;  
+  char buffer[4];
+  buffer[0] = b1;
   buffer[1] = b2;
   buffer[2] = b3;
   buffer[3] = '\0';
@@ -168,10 +185,33 @@ void screensplash(){
   display.clearDisplay();
 }
 
-void imprimirTexto(char txt[]){
+void printCpuPercent(int x, int y, char b1, char b2, char b3){
+  char buffer[4];
+  buffer[0] = b1;
+  buffer[1] = b2;
+  buffer[2] = b3;
+  buffer[3] = '\0';
+  display.setTextSize(1);
+  display.setCursor(x, y);
+  display.println(buffer);
+}
+
+void printMem(int x, int y, char b1, char b2, char b3, char b4){
+  char buffer[5];
+  buffer[0] = b1;
+  buffer[1] = b2;
+  buffer[2] = b3;
+  buffer[3] = b4;
+  buffer[4] = '\0';
+  display.setTextSize(1);
+  display.setCursor(x, y);
+  display.println(buffer);
+}
+
+void monitorBase(){
   display.clearDisplay();
   display.setTextSize(1);
-  display.setCursor(0,0);
-  display.println(txt);
+  display.setCursor(0, 0);
+  display.println("CPU 0:   %\nCPU 1:   %\nCPU 2:   %\nCPU 3:   %\nuRAM:     MB\nfRAM:     MB");
   display.display();
 }
